c3764a3a1d6f99142e4e1d75038c9f3cadf87c8d
Jeffrey Caffrey-Hill <jeff@caffreyhill.com>
2023-02-26 13:27:33 -0500

    maint: Add JUnit validation test

---
+++ tests/integration.rs
@@ -0,0 +1,40 @@
+use std::io::Write;
+use std::process::{
+    Command,
+    Stdio
+};
+use git2::Repository;
+
+#[test]
+fn validate_xml() {
+   let output = Command::new(env!("CARGO_BIN_EXE_cargo-test-scripts"))
+       .stdout(Stdio::piped())
+       .output()
+       .expect("Failed to run");
+
+    let tmpdir = env!("CARGO_TARGET_TMPDIR");
+
+    let schema_repo_workspace = format!("{tmpdir}/JUnit-Schema");
+
+    std::fs::remove_dir_all(&schema_repo_workspace).expect("Failed to remove");
+    Repository::clone("https://github.com/windyroad/JUnit-Schema.git", 
+        &schema_repo_workspace).expect("Failed to clone");
+    
+    let mut xmllint = Command::new("xmllint")
+        .arg("--schema")
+        .arg(format!("{schema_repo_workspace}/JUnit.xsd"))
+        .arg("-")
+        .stdout(Stdio::null())
+        .stderr(Stdio::piped())
+        .stdin(Stdio::piped())
+        .spawn()
+        .expect("Failed to lint");
+
+    let input = xmllint.stdin.as_mut().unwrap();
+    input.write_all(&output.stdout).expect("Failed to write to stdin");
+    drop(input);
+
+    let output = xmllint.wait_with_output().expect("Failed to wait");
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    assert!(output.status.success(), "{stderr}");
+}
